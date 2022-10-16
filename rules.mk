c6cc9c2cea2de19466ac070ace3002ba6022c109
shaunsingh <shaunsingh0207@gmail.com>
2022-10-16 04:13:15 -0400

    initial commit

--- rules.mk
+++ rules.mk
@@ -1,25 +1,44 @@
 SRC = matrix.c \
       i2c_master.c
 
+CUSTOM_MATRIX = lite
+
 # MCU name
 MCU = atmega328p
 
 # Bootloader selection
 BOOTLOADER = usbasploader
 
-# Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+# Default Build Options
+MAGIC_ENABLE = no           # Disable bootmagic
+NKRO_ENABLE = no            # Nkey Rollover (unsupported on VUSB)
+MOUSEKEY_ENABLE = no        # Mouse keys
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-CUSTOM_MATRIX = lite
+# Usb Nkey Rollover + audio/system controls
+EXTRAKEY_ENABLE = yes
 
+# VIAL
+VIA_ENABLE = yes            # Enable VIA support
+VIAL_ENABLE = yes           # Enable VIAL support
+VIAL_INSECURE = yes         # My keyboard is insecure :(
 
+# OLED
+OLED_ENABLE = yes           # Enable OLED
+OLED_DRIVER = SSD1306       # 128x64 OLED Driver
+WPM_ENABLE = yes            # Enable calculation of WPM
+
+# Haptics
+HAPTIC_DRIVER = DRV2605L    # Pimoroni Haptics
+
+# Dynamic Macros
+DYNAMIC_MACRO_ENABLE = yes  # Live Macro Recording
+
+# Space saving
+LTO_ENABLE = yes            # Link Time Optimization
+COMBO_ENABLE = no           # Disable Dynamic Combos 
