4d08c204ff661e8d60b82964415a8c21f33de7db
shaunsingh <shaunsingh0207@gmail.com>
2022-10-20 18:40:20 -0400

    (makefile) fixes

--- rules.mk
+++ rules.mk
@@ -1,24 +1,3 @@
-SRC = matrix.c \
-      i2c_master.c
-
-CUSTOM_MATRIX = lite
-
-# MCU name
-MCU = atmega328p
-
-# Bootloader selection
-BOOTLOADER = usbasploader
-
-# Default Build Options
-MAGIC_ENABLE = no           # Disable bootmagic
-NKRO_ENABLE = no            # Disable Nkey Rollover (unsupported on VUSB)
-CONSOLE_ENABLE = no         # Disable console for debug
-COMMAND_ENABLE = no         # Disable commands for debug and configuration
-SLEEP_LED_ENABLE = no       # Disable breathing sleep LED during USB suspend
-BACKLIGHT_ENABLE = no       # Disable backlight functionality
-RGBLIGHT_ENABLE = no        # Disable RGB underglow
-SPACE_CADET_ENABLE = no     # Disable wierd parenthesis shift thing we don't need
-
 # Audio/system controls
 EXTRAKEY_ENABLE = yes
 
@@ -36,6 +15,7 @@ DYNAMIC_MACRO_ENABLE = yes  # Live Macro Recording
 
 # Space saving
 LTO_ENABLE = yes            # Enable Link Time Optimization
+SPACE_CADET_ENABLE = no     # Disable wierd parenthesis shift thing we don't need
 KEY_OVERRIDE_ENABLE = no    # Key overrides just feel weird
 
 # OLED
