3e67fe252ca3a4e27c8a83a5443f107de0f983f3
shaunsingh <shaunsingh0207@gmail.com>
2022-10-20 19:45:01 -0400

    (please work)

--- rules.mk
+++ rules.mk
@@ -1,18 +1,18 @@
 # Audio/system controls
-EXTRAKEY_ENABLE = no
+EXTRAKEY_ENABLE = yes
 
 # Mouse keys
-MOUSEKEY_ENABLE = no
+MOUSEKEY_ENABLE = yes
 
 # Audio
 AUDIO_ENABLE = no
 
 # Haptics
-HAPTIC_ENABLE = no          # Enable Haptics
+HAPTIC_ENABLE = yes         # Enable Haptics
 HAPTIC_DRIVER = DRV2605L    # Pimoroni Haptics
 
 # Dynamic Macros
-DYNAMIC_MACRO_ENABLE = no   # Live Macro Recording
+DYNAMIC_MACRO_ENABLE = yes  # Live Macro Recording
 
 # Space saving
 LTO_ENABLE = yes            # Enable Link Time Optimization
@@ -20,10 +20,10 @@ SPACE_CADET_ENABLE = no     # Disable wierd parenthesis shift thing we don't nee
 KEY_OVERRIDE_ENABLE = no    # Key overrides just feel weird
 
 # OLED
-OLED_ENABLE = no            # Enable OLED
+OLED_ENABLE = yes           # Enable OLED
 OLED_DRIVER = SSD1306       # 128x64 OLED Driver
 
 # VIAL
-VIA_ENABLE = yes            # Enable VIA support
-VIAL_ENABLE = yes           # Enable VIAL support
-VIAL_INSECURE = yes         # My keyboard is insecure :(
+# VIA_ENABLE = yes            # Enable VIA support
+# VIAL_ENABLE = yes           # Enable VIAL support
+# VIAL_INSECURE = yes         # My keyboard is insecure :(
