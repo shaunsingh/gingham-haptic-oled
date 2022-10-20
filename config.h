dc3cf34f07b238b3fd40e01bb93c0ff28a246a31
shaunsingh <shaunsingh0207@gmail.com>
2022-10-20 19:48:58 -0400

    (please work)

--- config.h
+++ config.h
@@ -1,8 +1,7 @@
 #pragma once
 
 // every vial keyboard needs a unique UID
-#define VIAL_KEYBOARD_UID                                                      \
-  { 0x21, 0x72, 0x62, 0xA2, 0x6D, 0xD7, 0x3C, 0x39 }
+#define VIAL_KEYBOARD_UID { 0x21, 0x72, 0x62, 0xA2, 0x6D, 0xD7, 0x3C, 0x39 }
 
 // general settings
 #define TAPPING_TERM 170
@@ -12,8 +11,7 @@
 // haptic driver settings
 #ifdef HAPTIC_ENABLE
 #define FB_ERM_LRA 1
-#define FB_BRAKEFACTOR                                                         \
-  3 // For 1x:0, 2x:1, 3x:2, 4x:3, 6x:4, 8x:5, 16x:6, Disable Braking:7
+#define FB_BRAKEFACTOR 3 // For 1x:0, 2x:1, 3x:2, 4x:3, 6x:4, 8x:5, 16x:6, Disable Braking:7
 #define FB_LOOPGAIN 1 // For  Low:0, Medium:1, High:2, Very High:3
 #define RATED_VOLTAGE 2
 #define V_PEAK 2.85
--- rules.mk
+++ rules.mk
@@ -8,22 +8,22 @@ MOUSEKEY_ENABLE = yes
 AUDIO_ENABLE = no
 
 # Haptics
-HAPTIC_ENABLE = yes         # Enable Haptics
+HAPTIC_ENABLE = no          # Enable Haptics
 HAPTIC_DRIVER = DRV2605L    # Pimoroni Haptics
 
 # Dynamic Macros
-DYNAMIC_MACRO_ENABLE = yes  # Live Macro Recording
+DYNAMIC_MACRO_ENABLE = no   # Live Macro Recording
 
 # Space saving
-LTO_ENABLE = yes            # Enable Link Time Optimization
+LTO_ENABLE = no             # Enable Link Time Optimization
 SPACE_CADET_ENABLE = no     # Disable wierd parenthesis shift thing we don't need
 KEY_OVERRIDE_ENABLE = no    # Key overrides just feel weird
 
 # OLED
-OLED_ENABLE = yes           # Enable OLED
+OLED_ENABLE = no            # Enable OLED
 OLED_DRIVER = SSD1306       # 128x64 OLED Driver
 
 # VIAL
-# VIA_ENABLE = yes            # Enable VIA support
-# VIAL_ENABLE = yes           # Enable VIAL support
-# VIAL_INSECURE = yes         # My keyboard is insecure :(
+VIA_ENABLE = yes            # Enable VIA support
+VIAL_ENABLE = yes           # Enable VIAL support
+VIAL_INSECURE = yes         # My keyboard is insecure :(
