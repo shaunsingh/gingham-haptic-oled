a1f2e82d1ec26130c6dd33c9f80452530d696a3b
shaunsingh <shaunsingh0207@gmail.com>
2022-10-20 18:58:00 -0400

    (keymap.c) oled fixes

--- keymap.c
+++ keymap.c
@@ -165,33 +165,48 @@ void render_logo(void) {
 
 // Handle the main oled displays
 void render_status(void) {
-    // vertical seperator 1/2
+    // variables
     static const char PROGMEM sep_v[] = {0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0xC0, 0};
+    static const char PROGMEM os_m_1[] = {0x95, 0x96, 0};
+    static const char PROGMEM os_w_1[] = {0x97, 0x98, 0};
+    static const char PROGMEM sep_h1[] = {0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0};
+    static const char PROGMEM face_1[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0xE1, 0};  
+    static const char PROGMEM os_m_2[] = {0xB5, 0xB6, 0};
+    static const char PROGMEM os_w_2[] = {0xB7, 0xB8, 0};
+    static const char PROGMEM face_2[] = {0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xE1, 0}; 
+    static const char PROGMEM b_lock[] = {0xE1, 0xE1, 0};
+    static const char PROGMEM n_lock[] = {0x91, 0x92, 0};
+    static const char PROGMEM c_lock[] = {0x93, 0x94, 0};
+    static const char PROGMEM s_lock[] = {0x8F, 0x90, 0};
+    static const char PROGMEM sep_h2[] = {0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0};
+    #ifdef AUDIO_ENABLE 
+      static const char PROGMEM aud_en[] = {0xAF, 0xB0, 0};
+      static const char PROGMEM aud_di[] = {0xCF, 0xD0, 0};
+    #endif   
+    #ifdef HAPTIC_ENABLE
+      static const char PROGMEM hap_en[] = {0xB1, 0xB2, 0};
+    #endif   
+  
+    // vertical seperator 1/2
     oled_write_ln_P(sep_v, false);
 
     // draw 1/2 OS logo 
     if (keymap_config.swap_lctl_lgui) {
-        static const char PROGMEM os_m_1[] = {0x95, 0x96, 0};
         oled_write_P(os_m_1, false);
     } else {
-        static const char PROGMEM os_w_1[] = {0x97, 0x98, 0};
         oled_write_P(os_w_1, false);
     }
 
     // draw space seperator 1/2
-    static const char PROGMEM sep_h1[] = {0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0};
     oled_write_P(sep_h1, false);
 
     // draw 1/2 of face
-    static const char PROGMEM face_1[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0xE1, 0};  
     oled_write_P(face_1, false);
 
     // draw 2/2 of OS logo 
     if (keymap_config.swap_lctl_lgui) {
-        static const char PROGMEM os_m_2[] = {0xB5, 0xB6, 0};
         oled_write_P(os_m_2, false);
     } else {
-        static const char PROGMEM os_w_2[] = {0xB7, 0xB8, 0};
         oled_write_P(os_w_2, false);
     }
 
@@ -199,7 +214,6 @@ void render_status(void) {
     oled_write_P(sep_h1, false);
 
     // draw 2/2 of face
-    static const char PROGMEM face_2[] = {0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xE1, 0}; 
     oled_write_P(face_2, false);
 
     // draw vertical seperator 2/2
@@ -208,28 +222,23 @@ void render_status(void) {
     led_t led_usb_state = host_keyboard_led_state();
 
     // draw num/caps/scroll locks
-    static const char PROGMEM b_lock[] = {0xE1, 0xE1, 0};
     if (led_usb_state.num_lock) {
-        static const char PROGMEM n_lock[] = {0x91, 0x92, 0};
         oled_write_P(n_lock, false);
     } else {
         oled_write_P(b_lock, false);
     }
-    if (led_usb_state.caps_lock) {
-        static const char PROGMEM c_lock[] = {0x93, 0x94, 0};
+    if (led_usb_state.caps_lock) { 
         oled_write_P(c_lock, false);
     } else {
         oled_write_P(b_lock, false);
     }
     if (led_usb_state.scroll_lock) {
-        static const char PROGMEM s_lock[] = {0x8F, 0x90, 0};
         oled_write_P(s_lock, false);
     } else {
         oled_write_P(b_lock, false);
     }
 
     // draw space seperator (again)
-    static const char PROGMEM sep_h2[] = {0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0xE1, 0};
     oled_write_P(sep_h2, false);
 
     // draw haptic icon & audio icon
@@ -242,16 +251,13 @@ void render_status(void) {
 
     #ifdef AUDIO_ENABLE
         if (is_audio_on()) { 
-          static const char PROGMEM aud_en[] = {0xAF, 0xB0, 0};
           oled_write_P(aud_en, false); 
         } else {
-          static const char PROGMEM aud_di[] = {0xCF, 0xD0, 0};
           oled_write_P(aud_di, false);
         }
     #endif
 
      #ifdef HAPTIC_ENABLE
-        static const char PROGMEM hap_en[] = {0xB1, 0xB2, 0};
         oled_write_P(hap_en, false); 
      #endif
 }
