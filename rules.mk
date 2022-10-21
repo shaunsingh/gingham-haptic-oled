2e199616232d82b0ef02d884dc55bde76cc377a5
shaunsingh <shaunsingh0207@gmail.com>
2022-10-20 20:13:38 -0400

    disable VIAL and use qmk develop

--- rules.mk
+++ rules.mk
@@ -8,7 +8,6 @@ MOUSEKEY_ENABLE = yes
 AUDIO_ENABLE = no
 
 # Haptics
-HAPTIC_ENABLE = no          # Enable Haptics
 HAPTIC_DRIVER = DRV2605L    # Pimoroni Haptics
 
 # Dynamic Macros
@@ -20,10 +19,10 @@ SPACE_CADET_ENABLE = no     # Disable wierd parenthesis shift thing we don't nee
 KEY_OVERRIDE_ENABLE = no    # Key overrides just feel weird
 
 # OLED
-OLED_ENABLE = no            # Enable OLED
+OLED_ENABLE = yes           # Enable OLED
 OLED_DRIVER = SSD1306       # 128x64 OLED Driver
 
 # VIAL
-# VIA_ENABLE = yes            # Enable VIA support
-# VIAL_ENABLE = yes           # Enable VIAL support
-# VIAL_INSECURE = yes         # My keyboard is insecure :(
+# VIA_ENABLE = yes          # Enable VIA support
+# VIAL_ENABLE = yes         # Enable VIAL support
+# VIAL_INSECURE = yes       # My keyboard is insecure :(
