c236c0fe7f1a491616304d9c1a7e6478c3f1eb53
shaunsingh <shaunsingh0207@gmail.com>
2022-10-20 23:52:16 -0400

    haptics!

--- rules.mk
+++ rules.mk
@@ -1,13 +1,9 @@
 SRC = matrix.c \
       i2c_master.c
 
-# MCU name
 MCU = atmega328p
-
-# Bootloader selection
 BOOTLOADER = usbasploader
 
-# Build Options
 MAGIC_ENABLE = no           # Disable bootmagic
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -16,23 +12,13 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
-# Audio/system controls
-EXTRAKEY_ENABLE = yes
-
-# Mouse keys
-MOUSEKEY_ENABLE = yes
-
-# Haptics
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
 HAPTIC_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
 HAPTIC_DRIVER = DRV2605L
-
-# OLED
-OLED_ENABLE = no            # Enable OLED
-
-# Dynamic Macros
-DYNAMIC_MACRO_ENABLE = no   # Live Macro Recording
-
-# Space saving
-LTO_ENABLE = yes            # Enable Link Time Optimization
+DYNAMIC_MACRO_ENABLE = yes
 
 CUSTOM_MATRIX = lite
