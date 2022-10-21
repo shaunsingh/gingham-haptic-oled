d77c90784d990e24b3bc9426cf4ef18f53ab1690
shaunsingh <shaunsingh0207@gmail.com>
2022-10-20 22:37:52 -0400

    disable bootmagic

--- rules.mk
+++ rules.mk
@@ -8,8 +8,7 @@ MCU = atmega328p
 BOOTLOADER = usbasploader
 
 # Build Options
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+MAGIC_ENABLE = no           # Disable bootmagic
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
